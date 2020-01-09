fa1479aa5d085dc48d2b05a421f5f22cab6e88a5
rushi444 <rushi44@gmail.com>
2020-01-08 22:25:00 -0800

    added bootstrap and basic styling

--- jotto/src/components/Congrats.js
+++ jotto/src/components/Congrats.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Congrats =  props => {
   return props.success ? (
-    <div data-test='component-congrats'>
+    <div data-test='component-congrats' className='alert alert-success'>
       <span data-test='congrats-message'>
         Congratulations! You guessed the word
       </span>
